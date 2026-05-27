84aa2d26f978de09ca9180a7b18f63fffbdcd235
DelightGroupLLC <sales@delightgroupllc.com>
2026-05-28 00:20:18 +0530

    update visuals

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -44,6 +44,20 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      keyframes: {
+        'marquee-left': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+        'marquee-right': {
+          '0%': { transform: 'translateX(-50%)' },
+          '100%': { transform: 'translateX(0%)' },
+        }
+      },
+      animation: {
+        'marquee-left': 'marquee-left 80s linear infinite',
+        'marquee-right': 'marquee-right 80s linear infinite',
+      },
     },
   },
   plugins: [],
