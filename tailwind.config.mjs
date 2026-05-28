9e645dc4fc236573da202887eaa486d0c1ad1fd8
DelightGroupLLC <sales@delightgroupllc.com>
2026-05-29 03:54:50 +0530

    v4

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -40,9 +40,12 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        '3xl': 'calc(var(--radius) + 4px)',
+        '2xl': 'calc(var(--radius) + 2px)',
+        'xl': 'var(--radius)',
+        'lg': 'calc(var(--radius) - 2px)',
+        'md': 'calc(var(--radius) - 4px)',
+        'sm': 'calc(var(--radius) - 6px)',
       },
       keyframes: {
         'marquee-left': {
